ab19ed8aee7ec81e07f175f1de77758c49bd4984
unknown <birau@endiio.com>
2025-07-15 10:14:18 +0200

    slider

--- docs/script.js
+++ docs/script.js
@@ -8,31 +8,60 @@ hamburger.addEventListener('click', () => {
 });
 
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: true,
+  fade: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    520: {
+      slidesPerView: 2,
     },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
 
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
-  });
+let autoScroll;
+let userHasInteracted = false;
+
+// Start auto-slide every 1 second
+function startAutoScroll() {
+  autoScroll = setInterval(() => {
+    if (!userHasInteracted) {
+      swiper.slideNext();
+    }
+  }, 1000);
+}
+
+// Stop auto-slide if user interacts
+function stopAutoScroll() {
+  if (!userHasInteracted) {
+    userHasInteracted = true;
+    clearInterval(autoScroll);
+  }
+}
+
+// Start autoplay initially
+startAutoScroll();
+
+// Detect user interaction
+swiper.on('touchStart', stopAutoScroll);
+swiper.on('slideChangeTransitionStart', stopAutoScroll);
+swiper.on('click', stopAutoScroll);
+swiper.on('navigationNext', stopAutoScroll);
+swiper.on('navigationPrev', stopAutoScroll);
